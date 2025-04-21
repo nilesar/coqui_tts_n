8dabf789659fd4543ccd18656bf7bfbaf14faf03
brown_blue_man <nilendu.adhikary.cd.cse23@itbhu.ac.in>
2025-04-21 21:28:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,4 +96,5 @@ def serve_audio():
 
 # 🟢 Run server
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))  # use PORT from Render
+    app.run(debug=False, host="0.0.0.0", port=port)
